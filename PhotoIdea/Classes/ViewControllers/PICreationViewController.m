1c5b17a0bdcdd0ccb445ea9fa870921f36c4786b
Longbob <b.longevialle@gmail.com>
2014-05-29 20:37:23 +0200

    Can only add idea when title and details are given

--- PhotoIdea/Classes/ViewControllers/PICreationViewController.m
+++ PhotoIdea/Classes/ViewControllers/PICreationViewController.m
@@ -6,7 +6,10 @@
 //  Copyright (c) 2013 Longbob. All rights reserved.
 //
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
 #import "PICreationViewController.h"
+
 #import "PIIdeaManager.h"
 #import "PIIdeaViewObject.h"
 
@@ -21,6 +24,32 @@
 
 @implementation PICreationViewController
 
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self configureReactiveDoneEnabling];
+}
+
+- (void)configureReactiveDoneEnabling
+{
+    id(^textValidator)(NSString *) = ^id(NSString *text) {
+        return @(text.length > 0);
+    };
+    
+    RACSignal *titleValidSignal = [self.titleTextField.rac_textSignal map:textValidator];
+    RACSignal *detailsValidSignal = [self.details.rac_textSignal      map:textValidator];
+    
+    [[RACSignal combineLatest:@[titleValidSignal,detailsValidSignal]
+                       reduce:^id(NSNumber *titleValid, NSNumber *detailsValid)
+                        {
+                           return @(titleValid.boolValue && detailsValid.boolValue);
+                        }]
+                subscribeNext:^(NSNumber *enabled)
+                        {
+                            self.done.enabled = enabled.boolValue;
+                        }];
+}
+
 #pragma mark - actions
 
 - (IBAction)cancel:(UIBarButtonItem *)sender {
@@ -32,12 +61,8 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"doneAddingIdea"]) {
-        PIIdeaViewObject *idea = [PIIdeaViewObject ideaWithTitle:self.titleTextField.text];
-        if (![self.details.text isEqualToString:@""]) {
-            idea.details = self.details.text;
-        }
-        [[PIIdeaManager sharedManager] addIdea:idea];
-        
+        [[PIIdeaManager sharedManager] addIdea:[PIIdeaViewObject ideaWithTitle:self.titleTextField.text
+                                                                       details:self.details.text]];
     }
 }
 
